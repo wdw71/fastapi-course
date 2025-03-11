11115bc3dd5c52267ba852531de1b3fad1450940
Dmitrii Vislobokov <myuzwd@outlook.com>
2025-03-11 05:45:29 +0300

    false changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from random import randrange
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
